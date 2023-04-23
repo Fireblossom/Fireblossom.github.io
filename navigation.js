b1abee3fa1138665c5d62d10cc74a13f5c88ac76
Fireblossom <Fireblossom@users.noreply.github.com>
2023-04-23 23:44:56 +0200

    add js file for nav-bar

---
+++ navigation.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch("navigation.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            const navBarContainer = document.getElementById('nav-bar');
+            navBarContainer.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
